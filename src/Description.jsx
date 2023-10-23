019388a19c35bcb6a7fd6f1c5b4af364c99e1e91
David Omonehin <david.omonehin@gmail.com>
2023-10-23 22:56:50 +0100

    second commit display and usestate

--- src/Description.jsx
+++ src/Description.jsx
@@ -1,18 +1,29 @@
-import React from "react"
+import React, { useState } from "react"
 
 export default function Description() {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleButtonClick = () => {
+    alert('You entered: ' + inputValue);
+  };
+
   return (
     <div className="description">
       <div className="base-apparel">
-        <h3>BASE APPAREL</h3>
+        <img src="../images/logo.svg" alt="" />
       </div>
-      <h1>WE'RE COMING SOON</h1>
+      <h1><span>W E ' R E</span> COMING SOON</h1>
       <h4> 
         Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.
       </h4>
       <div className="email">
-        <input type="text" />
-        <button>
+        <input
+          type="email"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Email Address"
+         />
+        <button onClick={handleButtonClick}>
           <img src="../images/icon-arrow.svg" alt="" />
         </button>
       </div>
